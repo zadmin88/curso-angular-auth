22e2736104c43a125e0d5b4987baddc606fde1a2
zadmin88 <zaimon88@gmail.com>
2023-05-25 13:35:28 -0500

    Get all boards for user

--- src/app/modules/boards/pages/boards/boards.component.ts
+++ src/app/modules/boards/pages/boards/boards.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
-import { faBox, faWaveSquare, faClock, faAngleUp, faAngleDown, faHeart, faBorderAll, faUsers, faGear } from '@fortawesome/free-solid-svg-icons';
+import { Component, OnInit } from '@angular/core';
+import {
+  faBox,
+  faWaveSquare,
+  faClock,
+  faAngleUp,
+  faAngleDown,
+  faHeart,
+  faBorderAll,
+  faUsers,
+  faGear,
+} from '@fortawesome/free-solid-svg-icons';
 import { faTrello } from '@fortawesome/free-brands-svg-icons';
+import { MeService } from '@services/me.service';
+import { Board } from '@models/board.model';
 
 @Component({
   selector: 'app-boards',
-  templateUrl: './boards.component.html'
+  templateUrl: './boards.component.html',
 })
-export class BoardsComponent {
-
+export class BoardsComponent implements OnInit {
+  boards: Board[] = [];
   faTrello = faTrello;
   faBox = faBox;
   faWaveSquare = faWaveSquare;
@@ -19,6 +31,14 @@ export class BoardsComponent {
   faUsers = faUsers;
   faGear = faGear;
 
-  constructor() { }
+  constructor(private meService: MeService) {}
 
+  ngOnInit(): void {
+    this.getMeBoards();
+  }
+  getMeBoards() {
+    this.meService.getMeBoards().subscribe((boards) => {
+      this.boards = boards;
+    });
+  }
 }
